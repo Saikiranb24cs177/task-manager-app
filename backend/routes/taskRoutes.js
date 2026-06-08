19c45502dba2fbfaf425a5ccd9b4bacb148773e8
saikiran Nagelli <saikirannagelli3@gmail.com>
2026-06-08 21:45:12 +0530

    fixed routes

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+let tasks = [];
+
 router.get("/", (req, res) => {
-  res.send("Task Routes Working");
+  res.json(tasks);
+});
+
+router.post("/", (req, res) => {
+  const newTask = {
+    _id: Date.now(),
+    title: req.body.title,
+  };
+
+  tasks.push(newTask);
+  res.json(newTask);
 });
 
 module.exports = router;
